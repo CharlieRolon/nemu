d1332daa7c77b15387c36ea891e01112bce0cc6f
tracer-ics2023 <tracer@njuics.org>
2024-02-22 20:32:58 +0800

    >  compile NEMU zy2342101 charlie Linux homelab 6.2.0-39-generic #40~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 16 10:53:04 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:32:58 up  9:28,  1 user,  load average: 0.59, 0.65, 0.62

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -39,7 +39,7 @@ static struct rule {
 
   {" +", TK_NOTYPE},    // spaces
   {"(0x)?[0-9a-fA-F]+", TK_NUM},   // numbers
-  {"$pc|$0|ra|sp|gp|tp|t0|t1|t2|s0|s1|a0|a1|a2|a3|a4|a5|a6|a7|s2|s3|s4|s5|s6|s7|s8|s9|s10|s11|t3|t4|t5|t6", TK_REG},
+  {"\\$pc|\\$0|ra|sp|gp|tp|t0|t1|t2|s0|s1|a0|a1|a2|a3|a4|a5|a6|a7|s2|s3|s4|s5|s6|s7|s8|s9|s10|s11|t3|t4|t5|t6", TK_REG},
   {"\\+", '+'},         // plus
   {"\\*", '*'},         // time || dereferencels
   {"\\-", '-'},         // minus
