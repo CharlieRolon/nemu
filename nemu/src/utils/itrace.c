0c77e8b15fe3c734e1182308a8594a75b64ba933
tracer-ics2023 <tracer@njuics.org>
2024-04-03 01:23:46 +0800

    >  compile NEMU zy2342101 charlie Linux homelab 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  01:23:46 up 2 days,  9:28,  1 user,  load average: 0.53, 0.93, 1.07

--- nemu/src/utils/itrace.c
+++ nemu/src/utils/itrace.c
@@ -2,6 +2,7 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <elf.h>
+#include <device/map.h>
 
 #define MAX_IRINGBUF 8
 #ifdef CONFIG_ITRACE
@@ -486,5 +487,13 @@ void trace_func_ret(paddr_t pc) {
 			trace_func_ret(ret_target);
 		}
 	}
+}
+
+void trace_dread(paddr_t addr, int len, IOMap *map) {
+	log_write("dtrace: read  %10s at  " FMT_PADDR " ,%d\n", map->name, addr, len);
+}
+
+void trace_dwrite(paddr_t addr, int len, word_t data, IOMap *map) {
+	log_write("dtrace: write %10s at " FMT_PADDR " ,%d with " FMT_WORD "\n", map->name, addr, len, data);
 }
 #endif
