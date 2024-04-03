803bfb15141103c5307c098772240f425fe4a38e
tracer-ics2023 <tracer@njuics.org>
2024-04-03 21:44:11 +0800

    >  compile NEMU zy2342101 charlie Linux homelab 6.5.0-21-generic #21~22.04.1-Ubuntu SMP PREEMPT_DYNAMIC Fri Feb  9 13:32:52 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:44:11 up 2 days, 11:12,  1 user,  load average: 1.33, 1.04, 0.87

--- abstract-machine/am/src/platform/nemu/ioe/gpu.c
+++ abstract-machine/am/src/platform/nemu/ioe/gpu.c
@@ -8,8 +8,8 @@ void __am_gpu_init() {
 
 void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   uint32_t screen_wh = inl(VGACTL_ADDR);
-  uint32_t h = screen_wh & 0xffff;
-  uint32_t w = screen_wh >> 16;
+  uint16_t h = screen_wh & 0xffff;
+  uint16_t w = screen_wh >> 16;
   *cfg = (AM_GPU_CONFIG_T) {
     .present = true, .has_accel = false,
     .width = w, .height = h,
@@ -17,7 +17,23 @@ void __am_gpu_config(AM_GPU_CONFIG_T *cfg) {
   };
 }
 
+#include <klib.h>
+
 void __am_gpu_fbdraw(AM_GPU_FBDRAW_T *ctl) {
+  int x = ctl->x;
+  int y = ctl->y;
+  int w = ctl->w;
+  int h = ctl->h;
+  if (!ctl->sync && (w==0 || h==0)) return;
+  uint32_t * pixels = (uint32_t *) ctl->pixels;
+  uint32_t *fb = (uint32_t *)(uintptr_t)FB_ADDR;
+  uint32_t W = inl(VGACTL_ADDR) >> 16;
+  for (int i = y; i < y+h; i++) {
+    for (int j = x; j < x+w; j++) {
+      fb[W*i+j] = pixels[w*(i-y)+(j-x)];
+    }
+  }
+
   if (ctl->sync) {
     outl(SYNC_ADDR, 1);
   }
